f499ba6207fbd05cb4dfa9a675afb55a73e5cdbc
adrianotheson <adrianocairesfilho@gmail.com>
2023-08-29 15:37:08 -0300

    base do projeto

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+  <div className="header">
+
+    <img className="logo" src="logo.png" alt="Logo">
+
+    <div className="lupa">
+      <input type="text" placeholder="Pesquisar">
+
+      <img className="imagem" src="search-icon.png" alt="Search">
+
+    </div>
+    <ul className="itens">
+      <li className="item"><a href="#">Início</a></li>
+      <li className="item"><a href="#">Artistas</a></li>
+      <li className="item"><a href="#">Obras</a></li>
+      <li className="item"><a href="#">Login</a></li>
+    </ul>
+  </div>
+</body>
+</html>
+
+
---
+++ src/componentes/main/Main.tsx
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+  <div className="main">
+    <h1 className="titulo">Portfólio de Manga do Kentaro Miura</h1>
+    <div className="artista">
+
+      <img className="artista-imagem" src="kentaro-miura.jpg" alt="Kentaro Miura"></img>
+
+      <p>Kentarō Miura (三浦 建太郎, Miura Kentarō), nascido em Chiba em 11 de julho de 1966 e falecido em 6 de maio de 2021, foi um mangaká japonês renomado. Ele ganhou grande reconhecimento por sua aclamada série de fantasia sombria, intitulada "Berserk", que teve seu início em 1989 e se estendeu até a data do falecimento de Miura em 2021.</p>
+    </div>
+    <h2>Projetos Recentes</h2>
+    <div className="titulo-manga">
+      <h3>Título: "Berserk"</h3>
+
+      <img className="artista-imagem" src="kentaro-miura.jpg" alt="Kentaro Miura"></img>
+
+      <p>Breve descrição: Berserk (ベルセルク Beruseruku) é uma série de mangá escrita e ilustrada por Kentaro Miura. Situado em um mundo de fantasia sombria inspirado na Europa medieval, a história gira em torno de Guts, um solitário mercenário, e Griffith, o líder de um bando de mercenários chamado de "Bando do Falcão".</p>
+    </div>
+    <div className="titulo-manga">
+      <h3>Título: "Ken e no Michi"</h3>
+
+      <img className="artista-imagem" src="kentaro-miura.jpg" alt="Kentaro Miura"></img>
+
+      <p>Breve descrição: Com muito orgulho a Graphite editora está lançando Ken no Michi - O caminho da espada, um compilado com histórias de samurais selecionadas pelo Mestre Julio Shimamoto, nossa edição será Oversize (24x34cm) onde contará com as histórias de: Musashi, O espadachim do diabo, Possessão demoníaca, Zatoichi, A morte do Samurai, O órfão, O dever do Samurai, Shi e A vingança ou Harakiri além de matreiras e uma entrevista inédita com autor.</p>
+    </div>
+    <div className="galeria">
+      <h2>Galeria</h2>
+
+      <img className="artista-imagem" src="kentaro-miura.jpg" alt="Kentaro Miura"></img>
+
+        <h1>Noa (One-shot)</h1>
+      <div className="galeria-descricao">
+        <p>Legenda: Um dos primeiros trabalhos do famoso mangaka Miura Kentaru. Segue o nosso herói solitário Noah (Noé), em um deserto pós-apocalíptico.</p>
+      </div>
+
+      <img className="artista-imagem" src="kentaro-miura.jpg" alt="Kentaro Miura"></img>
+      <h1>Noa (One-shot)</h1>
+      <div className="galeria-descricao">
+        <p>Legenda: É uma demonstração, mas ele cita o passado dele, as motivações, elementos como o stigma marcado em sua pele aparecem, já existe o conceito de ser "apenas um humano" e não conseguir lidar com a grandiosidade do horror cósmico, algo bastante lovecraftiano.</p>
+      </div>
+      
+      <img className="artista-imagem" src="kentaro-miura.jpg" alt="Kentaro Miura"></img>
+      <h1>Noa (One-shot)</h1>
+      <div className="galeria-descricao">
+        <p>Legenda: Iba, um vigoroso e jovem historiador, desaparece durante uma expedição solitária para estudar a Rota da Seda...</p>
+      </div>
+    </div>
+    <h2>Inspirações para criar o maior manga da minha vida e o mais conhecido pelo público</h2>
+    <p>As principais influências de Kentarō Miura na criação de "Berserk" foram três mangás clássicos...</p>
+  </div>
+</body>
+</html>
+
+
