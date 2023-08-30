6cfe3f619fdc7a14d39afab5919feea3c0ad982d
mesadelaranja <adriano.filho@estudante.ifms.edu.br>
2023-08-30 07:48:26 -0400

    ajuda do batman

--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -1,27 +1,45 @@
+import "./Footer.css";
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-</head>
-<body>
-  <div className="footer">
-    <div className="contato">
-      <p>Contato</p>
-      <div className="reso">
-        <p>Email: <a className="link" href="mailto:Kentaro.Miuraemail@example.com">Kentaro.Miuraemail@example.com</a></p>
+export default function Footer() {
+  return (
+    <body>
+      <div className="footer">
+        <div className="contato">
+          <p>Contato</p>
+          <div className="reso">
+            <p>
+              Email:{" "}
+              <a className="link" href="mailto:Kentaro.Miuraemail@example.com">
+                Kentaro.Miuraemail@example.com
+              </a>
+            </p>
+          </div>
+          <div className="reso">
+            <p>
+              Instagram:{" "}
+              <a
+                className="link"
+                href="https://www.instagram.com/Kentaro.Miura_perfil"
+                target="_blank"
+              >
+                @Kentaro.Miura_perfil
+              </a>
+            </p>
+          </div>
+          <div className="reso">
+            <p>
+              Website:{" "}
+              <a
+                className="link"
+                href="http://www.KentaroMangasite.com"
+                target="_blank"
+              >
+                www.KentaroMangasite.com
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="reso">
-        <p>Instagram: <a className="link" href="https://www.instagram.com/Kentaro.Miura_perfil" target="_blank">@Kentaro.Miura_perfil</a></p>
-      </div>
-      <div className="reso">
-        <p>Website: <a className="link" href="http://www.KentaroMangasite.com" target="_blank">www.KentaroMangasite.com</a></p>
-      </div>
-    </div>
-  </div>
-</body>
-</html>
-
-
+    </body>
+  );
+}
--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,31 +1,31 @@
+import "./Header.css";
 
+export default function Header() {
+  return (
+    <body>
+      <div className="header">
+        <img className="logo" src="/logo.jpg" alt="Logo" />
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <div className="lupa">
+          <input type="text" placeholder="Pesquisar" />
 
-</head>
-<body>
-  <div className="header">
-
-    <img className="logo" src="logo.png" alt="Logo">
-
-    <div className="lupa">
-      <input type="text" placeholder="Pesquisar">
-
-      <img className="imagem" src="search-icon.png" alt="Search">
-
-    </div>
-    <ul className="itens">
-      <li className="item"><a href="#">Início</a></li>
-      <li className="item"><a href="#">Artistas</a></li>
-      <li className="item"><a href="#">Obras</a></li>
-      <li className="item"><a href="#">Login</a></li>
-    </ul>
-  </div>
-</body>
-</html>
-
-
+          <img className="imagem" src="/" alt="Search" />
+        </div>
+        <ul className="itens">
+          <li className="item">
+            <a href="#">Início</a>
+          </li>
+          <li className="item">
+            <a href="#">Artistas</a>
+          </li>
+          <li className="item">
+            <a href="#">Obras</a>
+          </li>
+          <li className="item">
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </body>
+  );
+}
